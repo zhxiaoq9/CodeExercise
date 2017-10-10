01d963276301e79d9bcdbf657e9d7c65cc64da5e
zhxiaoq9 <zhxiaoq9@foxmail.com>
2017-10-10 11:18:54 +0800

    Update Sorts.cpp

--- algorithm/Sorts.cpp
+++ algorithm/Sorts.cpp
@@ -71,9 +71,84 @@ void insertSort(vector<int> &vec){
 	}
 }
 
-//希尔排序
+//希尔排序:基本思想是将数组按照步长进行分组,组内进行插入排序
+//逐渐缩小分组直到步长为1为止
+void shellSort(vector<int> &vec){
+	int len = vec.size();
+	if (len < 2)
+		return;
+	//gap为步长,也是分组组数,gap减小到1时结束
+	for (int gap = len / 2; gap > 0; gap /= 2){
+		//总共gap组,每组组内进行插入排序
+		for (int i = 0; i < gap; i++){
+			//开始插入排序
+			for (int j = i + gap; j < len; j += gap){
+				//如果待排序的元素小于签名的元素则,找到合适的插入位置
+				if (vec[j] < vec[j - gap]){
+					int tmp = vec[j]; //保存当前元素
+					int k = j - gap;  //k指向已排好元素的最后一个元素
+					//若已排好元素大于待排元素则将其向后移动,并将k指向再前一个元素
+					while (k >= 0 && vec[k] > tmp){
+						vec[k + gap] = vec[k];
+						k -= gap;
+					}
+					//最后k指向了第一个小于等于待排序元素的位置,所以要再加上步长值
+					vec[k + gap] = tmp;
+				}
+			}
+		}
+	}
 
+}
+
+//合并:归并排序中的合并过程
+void merge(vector<int> &vec, int start, int end){
+	//保存归并排序后的数据
+	vector<int> merge_tmp(vec);
+	//从mid开始分为两个数组区间
+	int mid = start + (end - start + 1) / 2;
+	int i = start;
+	int j = mid + 1;
+	
+	//将两个数组归并
+	int index = 0;
+	while (i <= mid && j <= end){
+		if (vec[i] < vec[j])
+			merge_tmp[index++] = vec[i++];
+		else
+			merge_tmp[index++] = vec[j++];
+	}
+	//将未归并的数据合并到临时空间内
+	while (i <= mid)
+		merge_tmp[index++] = vec[i++];
+	while (j <= end)
+		merge_tmp[index++] = vec[j++];
+	//将临时空间中的数据再恢复到原来的空间
+	for (int i = 0; i < index; i++)
+		vec[start + i] = merge_tmp[i];
+}
 //归并排序
+void mergeSort(vector<int> &vec, int start, int end){
+	//递归终止条件1,如果只有一个元素则返回
+	if (end - start == 0){
+		return;
+	}
+	//递归终止条件2,如果有两个元素,则比较后返回
+	if (end - start == 1){
+		if (vec[start] > vec[end]){
+			swap(vec[start], vec[end]);
+		}
+		//无论是否交换都要返回
+		return;
+	}
+	//如果超过2个元素,则递归
+	int mid = start + (end - start + 1) / 2;
+	mergeSort(vec, start, mid);
+	mergeSort(vec, mid + 1, end);
+	
+	//递归完毕后进行数组合并
+	merge(vec, start, end);
+}
 
 //桶排序:思想是将数组位置看作一个个的桶,其中装着元素的个数,如a[3] = 5就表示数组中值为3的元素有5个
 //桶排序适合元素最大值与最小值范围差别不大的数组排序,并且数组中不能有负值,如果有负值则需要找到最小
@@ -110,7 +185,9 @@ int main(){
 	//bubbleSort(vec);
 	//selectSort(vec);
 	//insertSort(vec);
-	bucketSort(vec);
+	//bucketSort(vec);
+	//mergeSort(vec, 0, vec.size() - 1);
+	shellSort(vec);
 	for (int i = 0; i < vec.size(); i++){
 		cout << vec[i] << " ";
 	}
